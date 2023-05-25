91e6b389a24c6e5c65fe6b9f14f45dd872aacd04
divineion <najima.guermoudi@gmail.com>
2023-05-25 11:14:48 +0200

    fix/main: rectif lien vers fichier css dans index.html

--- app.js
+++ app.js
@@ -6,24 +6,34 @@ fetch("data.json")
     console.log(data);
     for (const i in data) {
       const results = document.createElement("div");
+      const detailReport = document.querySelector("#detailReport");
+      const icon = document.createElement("img");
+      const textContainer = document.createElement("div");
+      const p1 = document.createElement("p");
+      var p2 = document.createElement("p");
+      const span = document.createElement("span");
+      var scores =
+        data[1].score + data[2].score + data[0].score + data[3].score;
+
       console.log(results);
       results.classList.add(data[i].category);
-      const icon = document.createElement("img");
       icon.src = data[i].icon;
       results.appendChild(icon);
-      const textContainer = document.createElement("div");
       results.appendChild(textContainer);
-      const p1 = document.createElement("p");
       textContainer.appendChild(p1);
       p1.textContent = data[i].category;
-      const detailReport = document.querySelector("#detailReport");
       detailReport.appendChild(results);
-      const p2 = document.createElement("p");
       p2.textContent = " /100";
+      p2.classList.add("greyparagraph");
       textContainer.appendChild(p2);
-      const span = document.createElement("span");
       p2.insertAdjacentElement("afterbegin", span);
       span.textContent = data[i].score;
-      let scores = new Array();
     }
+    const pastille = document.querySelector("#pastille");
+    const p3 = document.createElement("p");
+    pastille.appendChild(p3);
+    p3.textContent = parseInt(scores / 4);
+    const p4 = document.createElement("p");
+    pastille.appendChild(p4);
+    p4.textContent = "of 100";
   });
